6b4f783b68eef3fd8db609ec6c2b94a1ff3d7a3f
Oleh <oleh.levch3nko@gmail.com>
2023-11-15 19:41:10 +0200

    Fix environment configuration

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -3,21 +3,22 @@ import { DataSource } from "typeorm";
 import { Task } from "./entity/task.entity";
 import path from "path";
 
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_PORT);
-console.log(process.env.DB_USERNAME);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_NAME);
-
-console.log("Attempting to connect...");
+const config =
+  process.env.NODE_ENV === "production"
+    ? {
+        url: process.env.DATABASE_URL,
+      }
+    : {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT as unknown as number,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+      };
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT as unknown as number,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  ...config,
   logging: true,
   entities: [Task],
   migrations: [`${path.resolve(__dirname, "migrations")}`],
