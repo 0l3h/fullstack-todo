9d86f5de6dd74b844ef0e78688f562d31ba9a95c
Oleh <oleh.levch3nko@gmail.com>
2023-11-16 14:07:31 +0200

    Format some files

--- frontend/src/types/task.types.ts
+++ frontend/src/types/task.types.ts
@@ -1,10 +1,10 @@
 export interface Task extends TaskBody {
-    id: number;
-    status: "done" | "undone";
-    isDone: boolean;
+  id: number;
+  status: "done" | "undone";
+  isDone: boolean;
 }
 
 export interface TaskBody {
-    title: string;   
-    priority: number;
+  title: string;
+  priority: number;
 }
